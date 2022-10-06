cfefe00a00968f3d770a2463342dc697fc790187
EmmaKristiina <82311562+EmmaKristiina@users.noreply.github.com>
2022-10-06 16:49:18 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,42 +4,57 @@ https://stackoverflow.com/questions/59193983/leaflet-show-popup-on-hover-with-th
 
 import "./styles.css";
 
-const getData = async () => {
-  const url =
-    "https://geo.stat.fi/geoserver/wfs?service=WFS&version=2.0.0&request=GetFeature&typeName=tilastointialueet:kunta4500k&outputFormat=json&srsName=EPSG:4326";
-  const dataPromise = await fetch(url);
-  const data = await dataPromise.json();
-
-  InputToMap(data);
-};
-
-const InputToMap = (data) => {
-  let map = L.map("map", {
-    minZoom: -3
+if (document.readyState !== "loading") {
+  console.log("valmis");
+  initialize();
+} else {
+  document.addEventListener("DOMContetntLoaded", function () {
+    console.log("else valmis");
+    initialize();
   });
+}
 
-  let geoJson = L.geoJSON(data, {
-    onEachFeature: getName
-  }).addTo(map);
+function initialize() {
+  const getData = async () => {
+    const url =
+      "https://geo.stat.fi/geoserver/wfs?service=WFS&version=2.0.0&request=GetFeature&typeName=tilastointialueet:kunta4500k&outputFormat=json&srsName=EPSG:4326";
+    const dataPromise = await fetch(url);
+    const data = await dataPromise.json();
 
-  let osm = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    maxZoom: 15,
-    attribution: "© OpenStreetMap"
-  }).addTo(map);
+    InputToMap(data);
+  };
 
-  map.fitBounds(geoJson.getBounds());
-};
+  const InputToMap = (data) => {
+    let map = L.map("map", {
+      minZoom: -3,
+    });
 
-const getName = (feature, layer) => {
-  const name = feature.properties.nimi;
+    let geoJson = L.geoJSON(data, {
+      onEachFeature: getName,
+    }).addTo(map);
 
-  layer.bindPopup(name);
-  layer.on("mouseover", function (e) {
-    this.openPopup();
-  });
-  layer.on("mouseout", function (e) {
-    this.closePopup();
-  });
-};
+    let osm = L.tileLayer(
+      "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+      {
+        maxZoom: 15,
+        attribution: "© OpenStreetMap",
+      }
+    ).addTo(map);
 
-getData();
+    map.fitBounds(geoJson.getBounds());
+  };
+
+  const getName = (feature, layer) => {
+    const name = feature.properties.nimi;
+
+    layer.bindPopup(name);
+    layer.on("mouseover", function (e) {
+      this.openPopup();
+    });
+    layer.on("mouseout", function (e) {
+      this.closePopup();
+    });
+  };
+
+  getData();
+}
